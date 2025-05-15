a379fc3fb7e0907c776fbccaf64c928b274c7477
sieunp06 <psieun0606@gmail.com>
2025-05-15 15:43:21 +0900

    chore: 쓸데 없는 import 문 삭제

--- src/main/java/com/ssafy/questory/service/mail/MailContentBuilder.java
+++ src/main/java/com/ssafy/questory/service/mail/MailContentBuilder.java
@@ -1,6 +1,5 @@
 package com.ssafy.questory.service.mail;
 
-import com.ssafy.questory.dto.request.member.MemberEmailRequestDto;
 import com.ssafy.questory.dto.response.mail.MailResponseDto;
 
 public interface MailContentBuilder {
--- src/main/java/com/ssafy/questory/service/mail/PasswordResetService.java
+++ src/main/java/com/ssafy/questory/service/mail/PasswordResetService.java
@@ -1,7 +1,6 @@
 package com.ssafy.questory.service.mail;
 
 import com.ssafy.questory.domain.Member;
-import com.ssafy.questory.dto.request.member.MemberEmailRequestDto;
 import com.ssafy.questory.dto.response.mail.MailResponseDto;
 import com.ssafy.questory.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
