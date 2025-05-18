6489da1c0e48dc59850188fd013d32b951230392
sieunp06 <psieun0606@gmail.com>
2025-05-18 19:53:18 +0900

    refactor: 의미가 모호한 네이밍 수정
    following, followed가 한 번에 이해하기 어렵다 판단되어 이를 수정함.

--- src/main/java/com/ssafy/questory/domain/Friend.java
+++ src/main/java/com/ssafy/questory/domain/Friend.java
@@ -5,16 +5,16 @@ import lombok.Getter;
 
 @Getter
 public class Friend {
-    private String followingEmail;
-    private String followedEmail;
+    private String requesterEmail;
+    private String targetEmail;
     private FollowStatus status;
 
     protected Friend() {}
 
     @Builder
-    private Friend(String followingEmail, String followedEmail, FollowStatus status) {
-        this.followingEmail = followingEmail;
-        this.followedEmail = followedEmail;
+    private Friend(String requesterEmail, String targetEmail, FollowStatus status) {
+        this.requesterEmail = requesterEmail;
+        this.targetEmail = targetEmail;
         this.status = status;
     }
 }
