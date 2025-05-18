f37401c5c2a1428e283faa5ec80a4e93e93c769d
ahyoon99 <kimay9623@naver.com>
2025-05-18 11:02:17 +0900

    feat: swagger 코드 추가

--- src/main/java/com/ssafy/questory/controller/StampController.java
+++ src/main/java/com/ssafy/questory/controller/StampController.java
@@ -3,6 +3,8 @@ package com.ssafy.questory.controller;
 import com.ssafy.questory.config.jwt.JwtService;
 import com.ssafy.questory.dto.response.stamp.StampsResponseDto;
 import com.ssafy.questory.service.StampService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,14 +17,17 @@ import java.util.Map;
 @RestController
 @RequestMapping("/stamps")
 @RequiredArgsConstructor
+@Tag(name = "스탬프 API", description = "스탬프 조회 관련 API")
 public class StampController {
     private final StampService stampService;
     private final JwtService jwtService;
 
     @GetMapping("")
+    @Operation(summary = "스탬프 목록 조회", description = "로그인한 사용자가 보유한 스탬프 목록을 조회합니다.")
     public ResponseEntity<Map<String, Object>> findStamps(@RequestParam(defaultValue = "1") int page,
                                                           @RequestParam(defaultValue = "5") int size,
                                                           @RequestHeader("Authorization") String authorizationHeader) {
+
         String token = authorizationHeader.substring(7);
         String memberEmail = jwtService.extractUsername(token);
 
