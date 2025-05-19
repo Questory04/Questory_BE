aace6580d9381891ba09e605bd7a75dae9280f24
김아윤 <kimay9623@naver.com>
2025-05-19 11:18:11 +0900

    test: 스탬프 목록 조회 테스트 코드 작성

---
+++ src/test/java/com/ssafy/questory/service/StampServiceTest.java
@@ -0,0 +1,71 @@
+package com.ssafy.questory.service;
+
+import com.ssafy.questory.dto.response.stamp.StampsResponseDto;
+import com.ssafy.questory.repository.StampRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
+
+public class StampServiceTest {
+    private StampRepository stampRepository = mock(StampRepository.class);
+    private StampService stampService;
+
+    private String memberEmail = "test@ssafy.com";
+    private List<StampsResponseDto> mockStamps;
+
+    @BeforeEach
+    void setUp(){
+        stampService = new StampService(stampRepository);
+
+        StampsResponseDto stamp1 = StampsResponseDto.builder()
+                .title("title1")
+                .url("url1")
+                .date(LocalDate.of(2025, 5, 10))
+                .contentTypeName("contentType1")
+                .sidoName("sido1")
+                .difficulty("EASY")
+                .description("description1")
+                .build();
+
+        StampsResponseDto stamp2 = StampsResponseDto.builder()
+                .title("title2")
+                .url("url2")
+                .date(LocalDate.of(2025, 6, 25))
+                .contentTypeName("contentType2")
+                .sidoName("sido2")
+                .difficulty("HARD")
+                .description("description2")
+                .build();
+        mockStamps = Arrays.asList(stamp1, stamp2);
+    }
+
+    @Test
+    @DisplayName("스탬프 목록 조회 성공")
+    void findStamps_success() {
+        // given
+        int page = 1;
+        int size = 5;
+        int offset = (page - 1) * size;
+
+        when(stampRepository.findStamps(memberEmail, offset, size)).thenReturn(mockStamps);
+
+        // when
+        List<StampsResponseDto> result = stampService.findStamps(memberEmail, page, size);
+
+        // then
+        assertThat(result).isNotNull();
+        assertThat(result.size()).isEqualTo(mockStamps.size());
+        assertThat(result).isEqualTo(mockStamps);
+
+        // verify
+        verify(stampRepository, times(1)).findStamps(memberEmail, offset, size);
+    }
+}
