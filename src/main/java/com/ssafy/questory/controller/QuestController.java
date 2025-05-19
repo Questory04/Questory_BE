0d8e5a885ba828b1bcc2930d0055f22c05cc7dd6
김아윤 <kimay9623@naver.com>
2025-05-19 16:49:22 +0900

    feat: 퀘스트 등록 기능 swagger 코드 추가

--- src/main/java/com/ssafy/questory/controller/QuestController.java
+++ src/main/java/com/ssafy/questory/controller/QuestController.java
@@ -3,6 +3,8 @@ package com.ssafy.questory.controller;
 import com.ssafy.questory.config.jwt.JwtService;
 import com.ssafy.questory.dto.request.quest.QuestRequestDto;
 import com.ssafy.questory.service.QuestService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +15,13 @@ import java.util.Map;
 @RestController
 @RequestMapping("/quests")
 @RequiredArgsConstructor
+@Tag(name = "퀘스트 API", description = "퀘스트 관련 API")
 public class QuestController {
     private final QuestService questService;
     private final JwtService jwtService;
 
     @PostMapping("")
+    @Operation(summary = "퀘스트 등록", description = "로그인한 사용자가 새로운 퀘스트를 생성합니다.")
     public ResponseEntity<Map<String, String>> saveQuest(@RequestBody QuestRequestDto questRequestDto,
                                                          @RequestHeader("Authorization") String authorizationHeader){
         String token = authorizationHeader.substring(7);
