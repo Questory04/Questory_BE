23c1a1754225b983d0769bf535be54fcbfc85a5c
김아윤 <kimay9623@naver.com>
2025-05-20 09:34:10 +0900

    test: 퀘스트 수정 테스트 코드 작성

--- src/test/java/com/ssafy/questory/service/QuestServiceTest.java
+++ src/test/java/com/ssafy/questory/service/QuestServiceTest.java
@@ -69,4 +69,82 @@ class QuestServiceTest {
 
         verify(questRepository, never()).saveQuest(any(), any(), anyInt());
     }
+
+    @Test
+    @DisplayName("퀘스트 수정")
+    void modifyQuest_Success() {
+        QuestRequestDto validQuestRequestDto = QuestRequestDto.builder()
+                .attractionId(56644)
+                .title("테스트 퀘스트")
+                .questDescription("테스트 퀘스트 설명입니다.")
+                .difficulty("MEDIUM")
+                .isPrivate(false)
+                .stampDescription("테스트 스탬프 설명")
+                .build();
+        int questId = 1;
+        String memberEmail = "test@ssafy.com";
+
+        // given
+        when(questRepository.getAttractionById(validQuestRequestDto.getAttractionId())).thenReturn(1);
+        when(questRepository.getmemberEmailByQuestId(questId)).thenReturn(memberEmail);
+
+        // when
+        questService.modifyQuest(questId, validQuestRequestDto, memberEmail);
+
+        // then
+        verify(questRepository, times(1)).modifyQuest(questId, validQuestRequestDto);
+    }
+
+    @Test
+    @DisplayName("유효하지 않은 관광지 ID에 대한 퀘스트 생성")
+    void modifyQuest_WithInvalidAttractionId_ThrowsException() {
+        QuestRequestDto inValidQuestRequestDto = QuestRequestDto.builder()
+                .attractionId(1)
+                .title("테스트 퀘스트")
+                .questDescription("테스트 퀘스트 설명입니다.")
+                .difficulty("MEDIUM")
+                .isPrivate(false)
+                .stampDescription("테스트 스탬프 설명")
+                .build();
+        String memberEmail = "test@ssafy.com";
+        int questId = 1;
+
+        // given
+        when(questRepository.getAttractionById(inValidQuestRequestDto.getAttractionId())).thenReturn(0);
+
+        // when & then
+        CustomException exception = assertThrows(CustomException.class, () -> {
+            questService.modifyQuest(questId, inValidQuestRequestDto, memberEmail);
+        });
+
+        assertEquals(ErrorCode.ATTRACTION_NOT_FOUND, exception.getErrorCode());
+        verify(questRepository, never()).modifyQuest(anyInt(), any(QuestRequestDto.class));
+    }
+
+    @Test
+    @DisplayName("권한 없는 회원이 퀘스트 수정")
+    void modifyQuest_WithUnauthorizedMember_ThrowsException() {
+        QuestRequestDto validQuestRequestDto = QuestRequestDto.builder()
+                .attractionId(56644)
+                .title("테스트 퀘스트")
+                .questDescription("테스트 퀘스트 설명입니다.")
+                .difficulty("MEDIUM")
+                .isPrivate(false)
+                .stampDescription("테스트 스탬프 설명")
+                .build();
+        int questId = 1;
+        String memberEmail = "test@ssafy.com";
+
+        // given
+        when(questRepository.getAttractionById(validQuestRequestDto.getAttractionId())).thenReturn(1);
+        when(questRepository.getmemberEmailByQuestId(questId)).thenReturn("another@ssafy.com");
+
+        // when & then
+        CustomException exception = assertThrows(CustomException.class, () -> {
+            questService.modifyQuest(questId, validQuestRequestDto, memberEmail);
+        });
+
+        assertEquals(ErrorCode.UNAUTHORIZED_MEMBER, exception.getErrorCode());
+        verify(questRepository, never()).modifyQuest(anyInt(), any(QuestRequestDto.class));
+    }
 }
