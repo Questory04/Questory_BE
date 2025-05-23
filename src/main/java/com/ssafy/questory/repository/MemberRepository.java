630de3a6b0dd407f20999e1c90cafb40c99504ff
Sieun Park <psieun0606@gmail.com>
2025-05-23 10:54:38 +0900

    feat: 이메일을 통한 유저 검색 기능 구현

--- src/main/java/com/ssafy/questory/repository/MemberRepository.java
+++ src/main/java/com/ssafy/questory/repository/MemberRepository.java
@@ -2,9 +2,11 @@ package com.ssafy.questory.repository;
 
 import com.ssafy.questory.domain.Member;
 import com.ssafy.questory.dto.request.member.MemberModifyRequestDto;
+import com.ssafy.questory.dto.response.member.MemberSearchResponseDto;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 @Mapper
@@ -14,4 +16,6 @@ public interface MemberRepository {
     int changePassword(@Param("member") Member member, @Param("changedPassword") String changedPassword);
     int modify(Member member);
     int withdraw(Member member);
+    List<Member> searchByEmailWithPaging(@Param("email") String email, @Param("offset") int offset, @Param("limit") int limit);
+    int countByEmail(@Param("email") String email);
 }
