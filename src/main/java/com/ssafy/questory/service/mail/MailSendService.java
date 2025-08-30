2931e2ef1adecb4ae5df7716bfc82375d4e56ce2
sieunp06 <psieun0606@gmail.com>
2025-08-30 21:14:12 +0900

    refactor: 이메일 전송 비동기 처리 변경
    이메일 전송 기능이 동기로 순차적으로 실행되고 있었기 때문에 이를 비동기 처리로 전환함.

--- src/main/java/com/ssafy/questory/service/mail/MailSendService.java
+++ src/main/java/com/ssafy/questory/service/mail/MailSendService.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +16,7 @@ public class MailSendService {
     @Value("${spring.mail.username}")
     private String fromEmail;
 
+    @Async("emailExecutor")
     public void sendEmail(MailResponseDto mailResponseDto) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(mailResponseDto.getEmail());
